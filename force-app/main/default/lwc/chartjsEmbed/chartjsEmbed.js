e1ab9a6e5b5cae292937084170c1178fce8f8cfd
Aaron Winters <awinters@fairtradeusa.org>
2019-05-07 11:25:06 -0700

    created promise chain to correctly sequence pulling axis labels from Salesforce and using in chart creation

--- force-app/main/default/lwc/chartjsEmbed/chartjsEmbed.js
+++ force-app/main/default/lwc/chartjsEmbed/chartjsEmbed.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { LightningElement, track, wire, api } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 import getAxisLabels from "@salesforce/apex/ChartJSController.getAxisLabels";
 //import getChartDataset from '@salesforce/apex/ChartJSController.getChartDataset';
 import { loadScript } from 'lightning/platformResourceLoader';
@@ -8,29 +8,12 @@ import chartjs from '@salesforce/resourceUrl/chartjslibrary';
 export default class ChartjsEmbed extends LightningElement {
     @api recordId;
     
-    // get the dynamic axis labels for the Chart and set axisLabel parameter
-    @track labelError;
-    @track axisLabels;
-    @wire(getAxisLabels)
-    wiredAxisLabels({ error, data }) {
-        if(data) {
-            this.axisLabels = data;
-            console.log('axisLabels in wired function: ' + this.axisLabels);
-            this.labelError = undefined;
-        } else if (error) {
-            this.labelError = error;
-            this.axisLabels = undefined;
-        }
-    }
-
-
-    // non-wired objects and properties used for building the chart
     @track error;
     
-    testAxisLabels = ['Q3 2017', 'Q2 2018', 'Q3 2018'];
     chart;
     chartjsInitialized = false;
 
+    // config object structure based on chart.js docs
     config = {
         type: 'bar',
         data: {
@@ -72,28 +55,34 @@ export default class ChartjsEmbed extends LightningElement {
         }
     };
 
-    // Script to build and display the chart
+    // Chain of promises to update config object with data from Salesforce and build and display the chart
     renderedCallback() {
         if (this.chartjsInitialized) {
             return;
         }
         this.chartjsInitialized = true;
-        this.wiredAxisLabels();
 
-        loadScript(this, chartjs)
-            .then(() => {
-                console.log('axisLabels are: ' + this.axisLabels);
-                console.log('testAxisLabels are: ' + this.testAxisLabels);
-                console.log('Stringified version of axisLabels ' + JSON.stringify(this.axisLabels));
-                this.config.data.labels = this.testAxisLabels;
-                console.log('axis labels are: ' + this.config.data.labels)
-                const ctx = this.template
-                    .querySelector('canvas.bar')
-                    .getContext('2d');
-                this.chart = new window.Chart(ctx, this.config);
-            })
-            .catch(error => {
-                this.error = error;
-            });
+        // Get labels for x-axis from Salesforce and update config object with values
+        getAxisLabels()
+
+        .then(result => {
+            this.config.data.labels = result;
+            console.log('rendered callback axis labels: ' + this.config.data.labels);
+        }) 
+
+        // load the chartjs library from static resources
+        .then (loadScript(this, chartjs))
+
+        // create and display the chart
+        .then(() => {
+            console.log('chart axis labels are: ' + this.config.data.labels);
+            const ctx = this.template
+                .querySelector('canvas.bar')
+                .getContext('2d');
+            this.chart = new window.Chart(ctx, this.config);
+        })
+        .catch(error => {
+            this.error = error;
+        });
     }
 }
