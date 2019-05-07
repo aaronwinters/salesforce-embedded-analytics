abf1aa53e58cb8ef84094e65ed571a1a362f6cfc
Aaron Winters <awinters@fairtradeusa.org>
2019-05-07 09:50:53 -0700

    testing wire service to retrieve axis labels from apex method; running into issues related to timing of lifecycle hooks

--- force-app/main/default/lwc/chartjsEmbed/chartjsEmbed.js
+++ force-app/main/default/lwc/chartjsEmbed/chartjsEmbed.js
@@ -1,20 +1,40 @@
-import { LightningElement, track } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement, track, wire, api } from 'lwc';
+import getAxisLabels from "@salesforce/apex/ChartJSController.getAxisLabels";
+//import getChartDataset from '@salesforce/apex/ChartJSController.getChartDataset';
 import { loadScript } from 'lightning/platformResourceLoader';
 import chartjs from '@salesforce/resourceUrl/chartjslibrary';
 
 export default class ChartjsEmbed extends LightningElement {
+    @api recordId;
+    
+    // get the dynamic axis labels for the Chart and set axisLabel parameter
+    @track labelError;
+    @track axisLabels;
+    @wire(getAxisLabels)
+    wiredAxisLabels({ error, data }) {
+        if(data) {
+            this.axisLabels = data;
+            console.log('axisLabels in wired function: ' + this.axisLabels);
+            this.labelError = undefined;
+        } else if (error) {
+            this.labelError = error;
+            this.axisLabels = undefined;
+        }
+    }
+
+
+    // non-wired objects and properties used for building the chart
     @track error;
+    
+    testAxisLabels = ['Q3 2017', 'Q2 2018', 'Q3 2018'];
     chart;
     chartjsInitialized = false;
-
-    // segment1 = [90274, 3440, 0];
-    // segment2 = [0,0, 578880];
-    //dates = ['Q3 2017', 'Q2 2018', 'Q3 2018'];
 
     config = {
         type: 'bar',
         data: {
-            labels: ['Q3 2017', 'Q2 2018', 'Q3 2018'],
+            labels: [],
             datasets: [
                 {
                     label: 'Closed Won',
@@ -52,14 +72,21 @@ export default class ChartjsEmbed extends LightningElement {
         }
     };
 
+    // Script to build and display the chart
     renderedCallback() {
         if (this.chartjsInitialized) {
             return;
         }
         this.chartjsInitialized = true;
+        this.wiredAxisLabels();
 
         loadScript(this, chartjs)
             .then(() => {
+                console.log('axisLabels are: ' + this.axisLabels);
+                console.log('testAxisLabels are: ' + this.testAxisLabels);
+                console.log('Stringified version of axisLabels ' + JSON.stringify(this.axisLabels));
+                this.config.data.labels = this.testAxisLabels;
+                console.log('axis labels are: ' + this.config.data.labels)
                 const ctx = this.template
                     .querySelector('canvas.bar')
                     .getContext('2d');
