cc7b1145f6bf508093af71bd895e5df6bdfd1e5d
Aaron Winters <awinters@fairtradeusa.org>
2019-11-11 09:07:57 -0800

    update project api version

--- force-app/main/default/lwc/tableauJSEmbed/tableauJSEmbed.js
+++ force-app/main/default/lwc/tableauJSEmbed/tableauJSEmbed.js
@@ -1,3 +1,32 @@
 import { LightningElement } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import tableaujs from '@salesforce/resourceUrl/tableaujsapi230';
 
-export default class tableauJSEmbed extends LightningElement {}
+export default class tableauJSEmbed extends LightningElement {
+    // Initialize the javascript controls for Tableau
+    tableauVizInitialized = false;
+
+    renderedCallback() {
+        if (this.tableauVizInitialized) {
+            return;
+        }
+        this.tableauVizInitialized = true;
+        
+        // load the Tableau JS API file
+        loadScript(this, tableaujs)
+
+        // create and display the visualization
+        .then(() => {
+            
+            let vizDiv = document.getElementById('tableauViz');
+            let vizURL = 'https://public.tableau.com/views/TableauJavaScriptAPIExample/BarChart';
+            let options = {
+                height: '550px',
+                width: '1100px',
+                hideTabs: true,
+                hideToolbar: true
+            }
+            viz = new tableau.Viz(vizDiv, vizURL, options);
+        })
+    }
+}
